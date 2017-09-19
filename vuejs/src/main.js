b7ba3983d6fecc5f1d1495813fde04cfe408ed8f
Francis Cote <fcote@spektrummedia.com>
2017-09-19 09:23:57 -0400

    Removed test code from main.js

--- vuejs/src/main.js
+++ vuejs/src/main.js
@@ -5,8 +5,6 @@ import App from './App'
 import router from './router'
 import store from './store'
 
-import * as Proxies from './shared/service-clients/service-clients'
-
 import BootstrapVue from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
